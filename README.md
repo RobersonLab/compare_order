cb5862c93cd29b4c2e0cbd147ba8b6b8866de58e
Eli Roberson <eroberso@dom.wustl.edu>
2013-09-10 15:27:50 -0500

    compare_order.cpp updated to support SCARF and FASTQ. Updated functions and reporting for greater detail. std::string erases performed in place rather than with reassignment. Timing added. Version information prints as well. Updated README.md to reflect FASTQ and SCARF functionality.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ compare_order
 =============
 
 ## Purpose
-A simple C++ program for comparing the read identifiers of two Illumina formatted (single-line) sequencing files. Reads the files together line by line and compares their read identifiers. The goal is to confirm that reads are ordered the same in each file. Relies on the data being single-line format, and for the identifier to be separated from index/pair by a # (e.g. HWIFAKE:1:1:1:1#0/1).
+A simple C++ program for comparing the read identifiers of two sequencing files (Illumina formatted single-line or FASTQ format). It reads the files line by line together and compares their read identifiers. The goal is to confirm that reads are ordered the same in each file. The identifier must be separated from index/pair by a # character, e.g. HWIFAKE:1:1:1:1#0/1.
 
 ## Compiling
 
@@ -14,4 +14,10 @@ A simple C++ program for comparing the read identifiers of two Illumina formatte
     g++ -o compare_order -O3 compare_order.cpp
 	
 ### Running
-compare_order mate_1.txt mate_2.txt
+Requires specifying three parameters:
+--format scarf|fastq
+--file1 filename
+--file2 filename
+
+    compare_order --format scarf --file1 mate_1.txt --file2 mate_2.txt
+	compare_order --format fastq --file1 mate_1.fq --file2 mate_2.fq
--- compare_order.cpp
+++ compare_order.cpp
@@ -1,108 +1,273 @@
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <typeinfo>
 
-const std::string VERSION = "1.0.0";
+// constant info
+const std::string PROGNAME = "compare_order";
+const std::string VERSION = "1.0.1";
+const std::string USAGE = PROGNAME + std::string(" v") + VERSION + std::string("\nUsage:\ncompare_order --file1 in1.fq --file2 in2.fq --format fastq\ncompare_order --file1 in1.txt --file2 in2.txt --format scarf\n");
+
+// globals
+std::string file1;
+std::string file2;
+std::string format;
+
+void error( std::string const& err )
+{
+	std::cerr << err << std::endl;
+	std::exit(1);
+}
+
+template <class T>
+std::string convertToString( const T& t )
+{
+	std::stringstream ss;
+	if ( (ss << t).fail() )
+	{
+		std::string s = typeid(t).name();
+		error("Program died converting " + s + " to string");
+	}
+	return ss.str();
+};
+
+void parseOptions( int argCount, char *commands[] )
+{
+	if (argCount != 7)
+	{
+		error( "Incorrect number of parameters used!\n\n" + USAGE );
+	}
+
+	int cmdIndex = 0;
+
+	while (++cmdIndex < argCount)
+	{
+		if (std::strcmp(commands[cmdIndex], "--format") == 0)
+		{
+			if (++cmdIndex < argCount)
+			{
+				if (std::strcmp(commands[cmdIndex], "fastq") == 0 || std::strcmp(commands[cmdIndex], "scarf") == 0)
+				{
+					format = commands[cmdIndex];
+				}
+				else
+				{
+					error("--format [" + std::string(commands[cmdIndex]) + "] not recognized!");
+				}
+			}
+			else
+			{
+				error( "--format used but no format specified!" );
+			}
+		}
+		else if (std::strcmp(commands[cmdIndex], "--file1") == 0)
+		{
+			if (++cmdIndex < argCount)
+			{
+				file1 = commands[cmdIndex];
+			}
+			else
+			{
+				error( "--file1 used with no value specified!");
+			}
+		}
+		else if (std::strcmp(commands[cmdIndex], "--file2") == 0)
+		{
+			if (++cmdIndex < argCount)
+			{
+				file2 = commands[cmdIndex];
+			}
+			else
+			{
+				error( "--file2 used with no value specified!");
+			}
+		}
+		else
+		{
+			error("Command-line value [" + std::string(commands[cmdIndex]) + "] not recognized!");
+		}
+	}
+
+	if (format.empty() || file1.empty() || file2.empty())
+	{
+		error("Please specify --file1, --file2 and --format");
+	}
+
+	if (file1 == file2)
+	{
+		error("--file1 and --file2 have the same file name! Nothing to do.");
+	}
+
+	std::cout << PROGNAME << " v" << VERSION << std::endl;
+	std::cout << std::string("--file1: ") << file1 << std::endl;
+	std::cout << std::string("--file2: ") << file2 << std::endl;
+	std::cout << std::string("--format: ") << format << std::endl;
+}
 
 int main( int argc, char *argv[])
 {
-	unsigned long long int lineCount = 0;
+	clock_t tStart = std::clock();
 	
-	if (argc != 3)
-	{
-		std::cout << "Please specify file 1 and file 2: compare_order file1.txt file2.txt" << std::endl;
-		return 1;
-	}
-
-	std::string f1 = argv[1];
-	std::string f2 = argv[2];
-
-	std::ifstream IFH1( f1.c_str(), std::ios::in );
-	std::ifstream IFH2( f2.c_str(), std::ios::in );
+	unsigned long long int lineCount = 0;
+	unsigned long long int readCount = 0;
+	
+	parseOptions( argc, argv );
+	
+	std::ifstream IFH1( file1.c_str(), std::ios::in );
+	std::ifstream IFH2( file2.c_str(), std::ios::in );
 
 	if (!IFH1.good())
 	{
-		std::cout << "Couldn't open file 1 " << f1;
-		return 1;
+		error( "Couldn't open file 1 " + file1 ) ;
 	}
 
 	if (!IFH2.good())
 	{
-		std::cout << "Couldn't open file 2 " << f2;
-		return 1;
+		error( "Couldn't open file 2 " + file2 );
 	}
 
 	std::string buffer1;
 	std::string buffer2;
 
-	while (++lineCount)
+	if (format == std::string("scarf"))
 	{
-		if (IFH1.eof() && IFH2.eof())
+		while (++lineCount && ++readCount)
 		{
-			break;
-		}
-		else if (IFH1.eof())
-		{
-			std::cout << "File 1 ended but file 2 did not! Line " << lineCount << "." << std::endl;
-			return 1;
-		}
-		else if (IFH2.eof())
-		{
-			std::cout << "File 2 ended but file 1 did not! Line " << lineCount << "." << std::endl;
-			return 1;
-		}
-		
-		std::getline( IFH1, buffer1 );
-		std::getline( IFH2, buffer2 );
+			if (IFH1.eof() && IFH2.eof())
+			{
+				break;
+			}
+			else if (IFH1.eof())
+			{
+				error( "File 1 ended but file 2 did not! Line " + convertToString( lineCount ) + "." );
+			}
+			else if (IFH2.eof())
+			{
+				error( "File 2 ended but file 1 did not! Line " + convertToString( lineCount ) + "." );
+			}
+			
+			std::getline( IFH1, buffer1 );
+			std::getline( IFH2, buffer2 );
 
-		if (buffer1.empty() && buffer2.empty())
-		{
-			std::cout << "No input read from file 1 or file 2 at line " << lineCount << std::endl;
-			return 1;
-		}
-		else if (buffer1.empty())
-		{
-			std::cout << "File 1 did not read input at line " << lineCount << std::endl;
-			return 1;
-		}
-		else if (buffer2.empty())
-		{
-			std::cout << "File 2 did not read input at line " << lineCount << std::endl;
-			return 1;
-		}
+			if (buffer1.empty() && buffer2.empty())
+			{
+				continue;
+			}
+			else if (buffer1.empty())
+			{
+				error( "File 1 did not read input at line " + convertToString( lineCount ) );
+			}
+			else if (buffer2.empty())
+			{
+				error( "File 2 did not read input at line " + convertToString( lineCount ) );
+			}
 
-		size_t search_pos = buffer1.find_last_of( std::string("#") );
-		if (search_pos == std::string::npos)
-		{
-			std::cout << "No # in read 1" << std::endl;
-			return 1;
+			size_t search_pos = buffer1.find_last_of( std::string("#") );
+			if (search_pos == std::string::npos)
+			{
+				error( "No \'#\' character in read 1 at line " + convertToString(lineCount) );
+			}
+
+			buffer1.erase(search_pos);
+
+			search_pos = buffer2.find_last_of( std::string("#") );
+			if (search_pos == std::string::npos)
+			{
+				error( "No \'#\' character in read 2 at line " + convertToString(lineCount) );
+			}
+
+			buffer2.erase(search_pos);
+
+			if (buffer1 != buffer2)
+			{
+				error( "Read 1 ID != Read 2 ID at line " + convertToString( lineCount ) + "\nFile 1: " + buffer1 + "\nFile 2: " + buffer2 + "\n" );
+			}
+			
+			buffer1.clear();
+			buffer2.clear();
 		}
-
-		buffer1 = buffer1.erase(search_pos);
-
-		search_pos = buffer2.find_last_of( std::string("#") );
-		if (search_pos == std::string::npos)
-		{
-			std::cout << "No # in read 2" << std::endl;
-			return 1;
-		}
-
-		buffer2 = buffer2.erase(search_pos);
-
-		if (buffer1 != buffer2)
-		{
-			std::cout << "Read 1 ID != Read 2 ID at line " << lineCount << std::endl << "Buffer 1: " << buffer1 << std::endl << "Buffer 2: " << buffer2 << std::endl;
-			return 1;
-		}
-
-		buffer1.clear();
-		buffer2.clear();
 	}
+	else if (format == std::string("fastq"))
+	{
+		short int fqLine = 0;
+		
+		while (++fqLine && ++lineCount)
+		{
+			if (IFH1.eof() && IFH2.eof())
+			{
+				break;
+			}
+			else if (IFH1.eof())
+			{
+				error( "File 1 ended but file 2 did not! Line " + convertToString( lineCount ) + "." );
+			}
+			else if (IFH2.eof())
+			{
+				error( "File 2 ended but file 1 did not! Line " + convertToString( lineCount ) + "." );
+			}
+			
+			std::getline( IFH1, buffer1 );
+			std::getline( IFH2, buffer2 );
 
+			if (buffer1.empty() && buffer2.empty())
+			{
+				continue;
+			}
+			else if (buffer1.empty())
+			{
+				error( "File 1 did not read input at line " + convertToString( lineCount ) );
+			}
+			else if (buffer2.empty())
+			{
+				error( "File 2 did not read input at line " + convertToString( lineCount ) );
+			}
+
+			if (fqLine == 1)
+			{
+				size_t search_pos = buffer1.find_last_of( std::string("#") );
+				if (search_pos == std::string::npos)
+				{
+					error( "No \'#\' character in read 1 at line " + convertToString(lineCount) );
+				}
+
+				buffer1.erase(search_pos);
+
+				search_pos = buffer2.find_last_of( std::string("#") );
+				if (search_pos == std::string::npos)
+				{
+					error( "No \'#\' character in read 2 at line " + convertToString(lineCount) );
+				}
+
+				buffer2.erase(search_pos);
+
+				if (buffer1 != buffer2)
+				{
+					error( "Read 1 ID != Read 2 ID at line " + convertToString( lineCount ) + "\nFile 1: " + buffer1 + "\nFile 2: " + buffer2 + "\n" );
+				}
+			}
+			else if (fqLine == 4)
+			{
+				fqLine = 0;
+				++readCount;
+			}
+			
+			buffer1.clear();
+			buffer2.clear();
+		}
+	}
+	
 	IFH1.close();
 	IFH2.close();
+	
+	clock_t tEnd = std::clock();
 
-	std::cout << "All reads in sync. " << lineCount << " total reads processed in each file." << std::endl;
+	std::cout << std::string("All reads in sync. ") << readCount << std::string(" total reads processed in each file.") << std::endl;
+	std::cout << std::string("Analysis complete in ") << std::setprecision(3) << (static_cast<float>(tEnd)-static_cast<float>(tStart)) / CLOCKS_PER_SEC << std::string(" seconds") << std::endl;
 	
 	return 0;
 }
